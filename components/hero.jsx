643973d0973489f74a695e70de1c179b4de0eb63
Rushikesh-9124 <rushikesh6281@gmail.com>
2025-09-10 12:12:32 +0530

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -36,7 +36,7 @@ const HeroSection = () => {
           </p>
         </div>
         <div className="flex justify-center space-x-4">
-          <Link href="/dashboard">
+          <Link href="/onboarding">
             <Button className={"px-8"} size={"lg"}>
               Get Started
             </Button>
